50779dfb2af5ae2a18ec8f8d95d40692c5cfaaf5
Puthiyattu <jithinputhiyattu@gmail.com>
2020-01-04 21:52:59 +0530

    SignIn - SignUp page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SignIn from './components/sign-in/SignIn';
+import SignUp from './components/sign-up/SignUp';
 
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello World...!</h1>
+        <SignUp/>
     </div>
   );
 }
